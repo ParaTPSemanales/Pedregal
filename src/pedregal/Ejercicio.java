63574a2fddb1cec6d77724d0cb63afbf7d27431f
MatiiTomaino <matiastomaino@live.com.ar>
2017-09-15 08:27:58 -0300

    Se crean atributos del objeto, constructores, se lee el archivo de entrada y se define el pedregal, creando una matriz auxiliar que almacena la posicion de los peñascos

--- src/pedregal/Ejercicio.java
+++ src/pedregal/Ejercicio.java
@@ -1,10 +1,38 @@
 package pedregal;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Ejercicio {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private static Scanner sc;
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		sc = new Scanner (new File("pedregal.in"));	
+		pedregal pedregal = new pedregal ();
+		pedregal.setDx(sc.nextInt());
+		pedregal.setDy(sc.nextInt());
+		pedregal.setfCasa(sc.nextInt());
+		pedregal.setlCasa(sc.nextInt());
+		pedregal.setCantidadDePeñascos(sc.nextInt());
+		int [][]terreno=new int [pedregal.getDx()][pedregal.getDy()];
+		while (sc.hasNextInt()) {
+			int x=sc.nextInt();
+			int y=sc.nextInt();
+			terreno[x-1][y-1]=1;
+		}
+		
+		/* PARA VER MATRIZ DEL TERRENO CON PEÑASCOS*/
+		for (int i = 0; i <pedregal.getDx(); i++) {
+			for (int j = 0; j < pedregal.getDy(); j++) {
+				System.out.print(terreno[i][j] + "\t");
+			}
+		 System.out.println(" ");
+		}		
+		
+		
+		
+		}
 }
